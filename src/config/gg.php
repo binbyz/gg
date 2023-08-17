d384fa472c0f3c6b9c0394d09054d634062ef54e
byzz <eyedroot@gmail.com>
2023-08-17 15:37:53 +0900

    add config `model_query_listener`

--- src/config/gg.php
+++ src/config/gg.php
@@ -4,6 +4,7 @@ return [
     'enabled' => env('GG_ENABLED', true),
     'listeners' => [
         'http_response_listener' => env('GG_HTTP_RESPONSE_LISTENER', false),
+        'model_query_listener' => env('GG_MODEL_QUERY_LISTENER', false),
     ],
     'host' => env('GG_HOST', 'host.docker.internal'),
     'port' => env('GG_PORT', 21868),
