8b161e73fc38365856f89fa516a1fe7190884f7a
byzz <eyedroot@gmail.com>
2024-03-13 20:10:38 +0900

    refactor

--- src/ConfigVariableChecker.php
+++ src/ConfigVariableChecker.php
@@ -6,10 +6,6 @@ class ConfigVariableChecker
 {
     public static function is(ConfigVariables $config): bool
     {
-        if (! in_array($config, ConfigVariables::cases(), true)) {
-            return false;
-        }
-
         return (bool) config($config->value, false);
     }
 }
